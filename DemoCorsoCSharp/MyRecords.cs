457dec83ca12616ed925a458e85752032b37a701
salvatore <salvatore.sorrentino@live.com>
2023-05-11 09:58:11 +0200

    record in c#

---
+++ DemoCorsoCSharp/MyRecords.cs
@@ -0,0 +1,20 @@
+﻿namespace DemoCorsoCSharp;
+
+//public record Product
+//{
+//    public string? Name { get; init; }
+//    public int CategoryId { get; init; }
+
+//    //public Product(string? name, int categoryId)
+//    //{
+//    //    (Name, CategoryId) = (name, categoryId);
+//    //}
+     
+//    public Product(string? name, int categoryId)
+//        => (Name, CategoryId) = (name, categoryId);
+
+//    public void Deconstruct(out string? name, out int categoryId)
+//        => (name, categoryId) = (Name, CategoryId);
+//}
+
+public record Product(string? Name, int CategoryId);
--- DemoCorsoCSharp/Program.cs
+++ DemoCorsoCSharp/Program.cs
@@ -3,13 +3,39 @@ using DemoCorsoCSharp;
 
 Console.WriteLine("Hello, World!");
 
-var shapes = new List<Shape> {
-    new Rectangle(),
-    new Triangle(),
-    new Circle()
-};
+//var shapes = new List<Shape> {
+//    new Rectangle(),
+//    new Triangle(),
+//    new Circle()
+//};
 
-foreach (var shape in shapes)
-{
-    shape.Draw();
-}
+//foreach (var shape in shapes)
+//{
+//    shape.Draw();
+//}
+
+//var product = new Product
+//{
+//    Name = "Video Game",
+//    CategoryId = 1
+//};
+
+//var newProduct = product with { CategoryId = 2 };
+
+//var anotherProduct = newProduct with { CategoryId = 1 };
+
+//var eq1 = product.Equals(anotherProduct);
+//Console.WriteLine(eq1);
+
+//var eq2 = product ==  anotherProduct;
+//Console.WriteLine(eq2);
+
+var product = new Product("Video Game", 1);
+//string? a = "";
+//int b = 0;
+
+//product.Deconstruct(out a, out b);
+//Console.WriteLine(b);
+
+var (a, b) = product;
+Console.WriteLine(b);
