8f8157b0bdd86ca0d82bdec9299ab02eea55931e
salvatore <salvatore.sorrentino@live.com>
2023-05-30 08:31:06 +0200

    Rimozione codice commentato

--- DemoCorsoCSharp/Program.cs
+++ DemoCorsoCSharp/Program.cs
@@ -1,116 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-//using Bogus;
-//using DemoCorsoCSharp;
-
-//Console.WriteLine("Hello, World!");
-
-//var customer = GetCustomer();
-//Console.WriteLine(customer.EmailAddress);
-
-//var cart = AddRandomProducts();
-
-////var cart2 = new List<IProductModel>();
-
-//foreach (var product in cart)
-//{
-//    Console.WriteLine($"{product.Title} {product.HasOrderBeenCompleted}");
-//    product.ShipItem(customer);
-//}
-
-//CustomerModel GetCustomer()
-//{
-//    var faker = new Faker<CustomerModel>()
-//        .StrictMode(true)
-//        .RuleFor(c => c.Id,  f => f.Random.Number(1,1000))
-//        .RuleFor(c => c.FirstName, f => f.Random.String(10, 'a', 'z'))
-//        .RuleFor(c => c.LastName, f => f.Random.String(10, 'a', 'z'))
-//        .RuleFor(c => c.City, f => f.Random.String(15, 'a', 'z'))
-//        .RuleFor(c => c.EmailAddress, f => f.Internet.Email())
-//        .RuleFor(c => c.Phone, f => f.Phone.PhoneNumber())
-//        ;
-
-//    return faker.Generate();
-//}
-
-
-//List<PhysicalProductModel> AddRandomPhysicalProducts()
-//{
-//    var faker = new Faker<PhysicalProductModel>()
-//       .StrictMode(true)
-//       .RuleFor(c => c.Title, f => f.Random.String(10, 'a', 'z'))
-//       .RuleFor(c => c.HasOrderBeenCompleted, f => false);
-
-//    var lista = new List<PhysicalProductModel>();
-//    for (int i = 1; i < 50; i++)
-//    {
-//        lista.Add(faker.Generate());
-//    }
-//    return lista;
-//}
-
-
-//List<IProductModel> AddRandomProducts() {
-//    var faker = new Faker<PhysicalProductModel>()
-//       .StrictMode(true)
-//       .RuleFor(c => c.Title, f => f.Random.String(10, 'a', 'z'))
-//       .RuleFor(c => c.HasOrderBeenCompleted, f => false);
-
-//    var fakerDigital = new Faker<DigitalProductModel>()
-//       .StrictMode(true)
-//       .RuleFor(c => c.Title, f => f.Random.String(10, 'a', 'z'))
-//       .RuleFor(c => c.HasOrderBeenCompleted, f => false)
-//       .RuleFor(c => c.TotalDownloadLeft, f => 5);
-
-
-//    var lista = new List<IProductModel>();
-//    for (int i = 1; i < 25; i++)
-//    {
-//        lista.Add(faker.Generate());
-//        lista.Add(fakerDigital.Generate());
-//    }
-//    return lista;
-//}
-
-//using DemoCorsoCSharp;
-
-//ICollidable collidable = new Solid();
-//IUpdatable updatable = new Movable();
-//IVisible visible = new Visible();
-//var mago = new Mago(collidable, updatable, visible);
-//mago.Paint();
-//mago.Collide();
-//mago.Update();
-
-
-//using DemoCorsoCSharp.SOLID;
-
-//OpenInvoice firstInvoice = new ProposedInvoice();
-//OpenInvoice secondInvoice = new FinalInvoice();
-//OpenInvoice thirdInvoice= new SpecialInvoice();
-
-//using DemoCorsoCSharp.SOLID;
-
-////Apple apple = new Orange();
-////Console.WriteLine(apple.GetColor());
-
-//Fruit fruit = new Orange();
-//Console.WriteLine(fruit.GetColor());
-//fruit = new Apple();
-//Console.WriteLine(fruit.GetColor());
-
-//using DemoCorsoCSharp.SOLID;
-
-//var manager = new EmployeeManager();
-//manager.AddEmployee(new Employee { Name = "Luisa Bianchi", Gender = Gender.Female, Position = Position.Administrator });
-//manager.AddEmployee(new Employee { Name = "Mario Bianchi", Gender = Gender.Male, Position = Position.Administrator });
-
-
-//var statistics = new EmployeeStatistics(manager);
-//Console.WriteLine(statistics.CountFemaleManager());
-
-using DemoCorsoCSharp.DTO;
+﻿using DemoCorsoCSharp.DTO;
 using DemoCorsoCSharp.Models;
-using Microsoft.EntityFrameworkCore;
 
 var database = new NorthwindContext();
 
@@ -162,4 +51,11 @@ database.Categories.Add(newCategory);
 await database.SaveChangesAsync();
 
 
-
+public interface IDataRepository<T>
+{
+    IEnumerable<T> GetAll();
+    T Get(int id);
+    void Add(T item);
+    void Update(T item);
+    void Delete(T item);
+}
